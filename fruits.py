4c83d75ce3c9433b70a3a6d2028a479d056047da
Nishant Gupta <ngofficial99@gmail.com>
2024-02-08 20:20:00 +0530

    string operations

--- fruits.py
+++ fruits.py
@@ -2,3 +2,7 @@ fruits = "apple,banANA,WaterMelon"
 split_fruit = fruits.split(",")
 print(fruits)
 print(split_fruit)
+print(fruits.count("a"))
+upper_fruits=fruits.upper()
+print(upper_fruits)
+print(upper_fruits.count("A"))
